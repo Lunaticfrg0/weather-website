1d01d43bb9be86eaaa973ca0f0c1ebafdf0ed0c8
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-13 22:17:56 -0400

    Made changes to allow Heroku deploy

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) =>{
     firstMessage.textContent = "Loading response..."
     secondMessage.textContent = ""
     
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>{
+    fetch(`/weather?address=${location}`).then((response) =>{
     response.json().then((data)=>{
         if(data.error){
             firstMessage.textContent = data.error
